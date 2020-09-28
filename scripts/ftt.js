f34b53255d0a769ede3ccc55074e7cb90a022c71
Will <will@willm.xyz>
2020-09-28 16:16:25 +0000

    Initial commit

---
+++ scripts/ftt.js
@@ -0,0 +1,14 @@
+/* THIS CODE WAS MADE BY HOTFRONT WITH HELP FROM W3SCHOOLS */
+function tab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
